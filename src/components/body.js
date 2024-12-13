36d13181f29a01cee470200db4eb526b565b17dd
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2024-12-14 01:39:53 +0530

    Tailwind

--- src/components/body.js
+++ src/components/body.js
@@ -17,12 +17,12 @@ const Body = ()=>{
     return(  <>
 
 
-        <input type ="text" className="search" placeholder="Search" value={searchTxt} onChange={(e)=>{
+        <input type ="text" className="m-5 mx-5 bg-red-100 rounded-md " placeholder="Search" value={searchTxt} onChange={(e)=>{
             setSearchTxt(e.target.value);
         }}></input>
 
 
-        <button onClick={()=>{
+        <button className="m-3 p-2 rounded-md bg-violet-200" onClick={()=>{
            const data = filterfn(searchTxt,allRestroList);
             setRestroList(data);
             setSearchBol(true);
@@ -31,7 +31,7 @@ const Body = ()=>{
 
         { restroList.length === 0 ? ( searchBol ? (<div> Not found </div> ) : ( <Shimmer/> ) 
         ) : ( 
-           <div className="body">
+           <div className="flex flex-wrap">
             {
             restroList.map((restaurant)=>{
                 return <Link to={"/restaurant/" + restaurant.info.id} >< Restaurant_card{...restaurant.info} key={restaurant.info.id}/></Link>
@@ -44,7 +44,7 @@ const Body = ()=>{
 
 const Restaurant_card = ({name,avgRating,cuisines,cloudinaryImageId})=>{
     return(<>
-        <div className="card">
+        <div className="w-64 m-4 bg-gray-400 shadow-xl">
            <img src={IMG_CDN_RESTRO+cloudinaryImageId} />
             <h2>{name}</h2>
             <h3>{cuisines.join(",")}</h3>
--- src/components/header.js
+++ src/components/header.js
@@ -6,14 +6,14 @@ import useOnline from "./utils/useOnline";
 const Header = ()=>{
 
     return (
-      <div className="head">
-        <a href="/" ><img  src={IMG_LOGO}/> </a>
-        <ul >
-            {(useOnline())? ( <li>🟢 Online</li>) :(<li>🔴 Offline</li>)}
+      <div className=" flex p-2 m-2 justify-between bg-violet-200 rounded-md shadow-lg">
+        <a href="/" ><img className=" size-20 rounded-md hover:shadow-xl " src={IMG_LOGO}/> </a>
+        <ul className=" flex place-items-center " >
+            {(useOnline())? ( <li className="m-3 p-3 rounded-md bg-red-200" >🟢 Online</li>) :(<li className="m-3 p-3 rounded-md bg-pink-300" >🔴 Offline</li>)}
          
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/offer" >Offers</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
+            <li className="m-3 p-3 rounded-md hover:bg-pink-300"><Link to="/">Home</Link></li>
+            <li className="m-3 p-3 rounded-md hover:bg-pink-300"><Link to="/offer" >Offers</Link></li>
+            <li className="m-3 p-3 rounded-md hover:bg-pink-300"><Link to="/cart">Cart</Link></li>
         </ul>
         <Btn/>
       </div>
@@ -22,7 +22,7 @@ const Header = ()=>{
 
 const Btn = ()=>{
   const [log,setLog] = useState("Login")
-  return (<button onClick={()=> 
+  return (<button className= "m-3 p-3 rounded-md bg-gray-600 text-white hover:shadow-xl" onClick={()=> 
     { log=="Login"? (setLog("Loggout") ) :(setLog("Login"))} }>
     {log}
   </button>
